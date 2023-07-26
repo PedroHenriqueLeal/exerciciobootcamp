ef6d67b0145c097b640a5c9c1069a4262a07dd12
PedroHenriqueLeal <pedro.15.pl.pl@gmail.com>
2023-07-26 00:46:32 -0300

    fix: delete the comments

--- exercicio/Main.java
+++ exercicio/Main.java
@@ -1,21 +1,7 @@
 package exercicio;
+
 import java.util.Comparator;
 import java.util.List;
-
-// Listar todas as pessoas
-// Listar as 10 primeiras pessoas
-// Listar as pessoas que são do Brasil
-// Listar as pessoas do Brasil com o nome em Uppercase e suas idades
-// Listar as pessoas que são do Brasil, em ordem de nome
-// Listar as pessoas que são do Brasil, em ordem de idade
-// Contar quantas pessoas tem mais de 45 anos
-// Calcular a média de idade das pessoas
-
-// filter() --> seleciona apenas uma parte dos dados. Para isso a expressão lambda deve
-//    retornar verdadeiro ou falso.
-// verdadeiro para os itens que farão pate do resultado, e falso caso contrário
-
-// map() --> aplicado para transformar os dados de entrada, gerando um novo conjunto de saída
 
 public class Main {
     public static void main(String[] args) {
@@ -32,27 +18,27 @@ public class Main {
                 .forEach(System.out::println);
 
         // Listar as pessoas que são do Brasil:
-        System.out.println("\nPessoas do Brasil:");
+        System.out.println("\nLista de pessoas do Brasil:");
         pessoas.stream()
                 .filter(p -> p.getPais().equals("Brazil"))
                 .forEach(System.out::println);
 
         // Listar as pessoas do Brasil com o nome em Uppercase e suas idades:
-        System.out.println("\nPessoas do Brasil com nome maiusculo e idade:");
+        System.out.println("\nLista de pessoas do Brasil com nome maiusculo e idade:");
         pessoas.stream()
                 .filter(p -> p.getPais().equals("Brazil"))
                 .map( p -> p.getNome().toUpperCase() + ", " + p.getIdade())
                 .forEach(System.out::println);
 
         // Listar as pessoas que são do Brasil, em ordem de nome:
-        System.out.println("\nPessoas do Brasil em ordem de nome:");
+        System.out.println("\nLista de pessoas do Brasil em ordem de nome:");
         pessoas.stream()
                 .filter(p -> p.getPais().equals("Brazil"))
                 .sorted(Comparator.comparing(Pessoa::getNome))
                 .forEach(System.out::println);
 
         // Listar as pessoas que são do Brasil, em ordem de idade:
-        System.out.println("\nPessoas do Brasil em ordem de idade:");
+        System.out.println("\nLista de pessoas do Brasil em ordem de idade:");
         pessoas.stream()
                 .filter(p -> p.getPais().equals("Brazil"))
                 .sorted(Comparator.comparing(Pessoa::getIdade))
@@ -63,10 +49,10 @@ public class Main {
         long pessoasAcimaDe45Anos = pessoas.stream()
         .filter(p -> p.getIdade() > 45)
         .count();
-        System.out.println("\nQuantidade de pessoas com mais de 45 anos: " + pessoasAcimaDe45Anos);
+        System.out.println("\nQuantidade de pessoas da lista com mais de 45 anos: " + pessoasAcimaDe45Anos);
 
         // Listar quantas pessoas tem mais de 45 anos:
-        System.out.println("\nPessoas com mais de 45 anos:");
+        System.out.println("\nLista de pessoas com mais de 45 anos:");
         pessoas.stream()
                 .filter(p -> p.getIdade() > 45)
                 .forEach(System.out::println);
@@ -76,10 +62,10 @@ public class Main {
                 .mapToDouble(Pessoa::getIdade)
                 .average()
                 .orElse(0);
-        System.out.println("\nMédia de idade das pessoas: " + mediaIdade);
- }
-
-
+        System.out.println("\nMédia de idade das pessoas da lista: " + mediaIdade);
+        
     }
+
+ }
 
 
